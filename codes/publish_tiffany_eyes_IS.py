366e658d06adf01c9bc377e39538c4f3ee3c8c7c
Lab Penguin Team <lab.penguin.ifes@gmail.com>
2025-04-30 17:19:23 -0300

    Create publish_tiffany_eyes_IS.py
    le do HTTP da ESP32 e publica no Espaço Inteligente

---
+++ codes/publish_tiffany_eyes_IS.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+from is_wire.core import Channel, Message
+from is_msgs.image_pb2 import Image
+import requests
+
+'''
+INFO SECTION
+- if you want to monitor raw parameters of ESP32CAM, open the browser and go to http://192.168.x.x/status
+- command can be sent through an HTTP get composed in the following way http://192.168.x.x/control?var=VARIABLE_NAME&val=VALUE (check varname and value in status)
+'''
+
+# ESP32 URL
+URL = "http://10.10.1.231"
+AWB = True
+
+# Face recognition and opencv setup
+cap = cv2.VideoCapture(URL + ":81/stream")
+
+def to_image(input_image, encode_format='.jpeg', compression_level=0.8):
+    if isinstance(input_image, np.ndarray):
+        if encode_format == '.jpeg':
+            params = [cv2.IMWRITE_JPEG_QUALITY, int(compression_level * (100 - 0) + 0)]
+        elif encode_format == '.png':
+            params = [cv2.IMWRITE_PNG_COMPRESSION, int(compression_level * (9 - 0) + 0)]
+        else:
+            return Image()
+        cimage = cv2.imencode(ext=encode_format, img=input_image, params=params)
+        return Image(data=cimage[1].tobytes())
+    elif isinstance(input_image, Image):
+        return input_image
+    else:
+        return Image()
+
+channel = Channel('amqp://guest:guest@10.10.2.211:30000')
+
+def set_resolution(url: str, index: int=1, verbose: bool=False):
+    try:
+        if verbose:
+            resolutions = "10: UXGA(1600x1200)\n9: SXGA(1280x1024)\n8: XGA(1024x768)\n7: SVGA(800x600)\n6: VGA(640x480)\n5: CIF(400x296)\n4: QVGA(320x240)\n3: HQVGA(240x176)\n0: QQVGA(160x120)"
+            print("available resolutions\n{}".format(resolutions))
+
+        if index in [10, 9, 8, 7, 6, 5, 4, 3, 0]:
+            requests.get(url + "/control?var=framesize&val={}".format(index))
+        else:
+            print("Wrong index")
+    except:
+        print("SET_RESOLUTION: something went wrong")
+
+def set_quality(url: str, value: int=1, verbose: bool=False):
+    try:
+        if value >= 10 and value <=63:
+            requests.get(url + "/control?var=quality&val={}".format(value))
+    except:
+        print("SET_QUALITY: something went wrong")
+
+def set_awb(url: str, awb: int=1):
+    try:
+        awb = not awb
+        requests.get(url + "/control?var=awb&val={}".format(1 if awb else 0))
+    except:
+        print("SET_QUALITY: something went wrong")
+    return awb
+
+if __name__ == '__main__':
+    set_resolution(URL, index=8)
+    set_resolution(URL, index=9, verbose=True)
+    set_quality(URL, value=10)
+
+    while True:
+        if cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                img_msg = Message()
+                img_msg.pack(to_image(frame))
+
+                channel.publish(img_msg, topic='tiffanyEyes.Frame')
+            
